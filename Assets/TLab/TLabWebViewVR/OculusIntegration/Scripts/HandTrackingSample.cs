ea56ca80f1e516de95ff424b98090be4c9c124b6
TLabAltoh <121733943+TLabAltoh@users.noreply.github.com>
2024-02-13 14:06:50 +0900

    update plugin

--- Assets/TLab/TLabWebViewVR/OculusIntegration/Scripts/HandTrackingSample.cs
+++ Assets/TLab/TLabWebViewVR/OculusIntegration/Scripts/HandTrackingSample.cs
@@ -5,7 +5,6 @@ namespace TLab.XR.Oculus
 {
     public class HandTrackingSample : MonoBehaviour
     {
-        [SerializeField] private TLabWebViewVRTouchEventListener m_touchListener;
         [SerializeField] private TLabOVRInputModule m_inputModule;
 
         [SerializeField] private OVRHand m_leftHand;
@@ -14,8 +13,6 @@ namespace TLab.XR.Oculus
         void Start()
         {
             m_inputModule.joyPadClickButton = OVRInput.Button.One;  // pinch down
-
-            m_touchListener.touchButton = OVRInput.Button.One;
 
             m_inputModule.rayTransformRight = m_rightHand.PointerPose;
             m_inputModule.rayTransformLeft = m_rightHand.PointerPose;
