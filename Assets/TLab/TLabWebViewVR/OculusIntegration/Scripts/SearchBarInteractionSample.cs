a286d2d9eb9fb4f8d943f153caf0772309e81d14
TLabAltoh <121733943+TLabAltoh@users.noreply.github.com>
2024-03-03 05:16:27 +0900

    add ui interaction sample

---
+++ Assets/TLab/TLabWebViewVR/OculusIntegration/Scripts/SearchBarInteractionSample.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using TMPro;
+using TLab.Android.WebView;
+
+public class SearchBarInteractionSample : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI m_text;
+    [SerializeField] private TLabWebView m_webview;
+
+    /// <summary>
+    /// Press button to execute
+    /// </summary>
+    public void AddEventListener()
+    {
+        string jsCode = @"
+            var inputs = [];
+
+            function querySelectorAllForAllTree(node, id) {
+                if (node == null) {
+                    return;
+                }
+
+                if (node.shadowRoot != undefined && node.shadowRoot != null) {
+                    querySelectorAllForAllTree(node.shadowRoot, id);
+                }
+
+                for (var i = 0; i < node.childNodes.length; i++) {
+                    querySelectorAllForAllTree(node.childNodes[i], id);
+                }
+
+                if (typeof node.querySelector == 'function') {
+                    var element = node.querySelector(id);
+                    if (element != undefined && element != null)
+                    {
+                        if (!inputs.includes(element))
+                        {
+                            inputs.push(element);
+                        }
+                    }
+                }
+            }
+
+            querySelectorAllForAllTree(document, 'textarea');
+
+            for (const input of inputs) {
+                input.addEventListener('focusin', function() {
+                    window.TLabWebViewActivity.unitySendMessage('SearchBar Interaction Sample', 'OnMessage', 'Foucusin');
+                });
+
+                input.addEventListener('focusout', function() {
+                    window.TLabWebViewActivity.unitySendMessage('SearchBar Interaction Sample', 'OnMessage', 'Foucusout');
+                });
+            }
+
+            window.TLabWebViewActivity.unitySendMessage('SearchBar Interaction Sample', 'OnMessage', 'Executed');
+            ";
+
+        m_webview.EvaluateJS(jsCode);
+    }
+
+    public void OnMessage(string message)
+    {
+        Debug.Log(message);
+
+        m_text.text = message;
+    }
+}
